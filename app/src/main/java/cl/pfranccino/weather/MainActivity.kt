7ae1dfc1b323aed8d62eafdcb4a0408a93f98db7
pfranccino <paul.ayala023@gmail.com>
2024-09-23 16:25:46 -0300

    feat: added new gitignore and added first view components

--- app/src/main/java/cl/pfranccino/weather/MainActivity.kt
+++ app/src/main/java/cl/pfranccino/weather/MainActivity.kt
@@ -3,13 +3,17 @@ package cl.pfranccino.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cl.pfranccino.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +21,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InformationItemNews(title: String, subtitle: String) {
+    Column(
+        modifier = Modifier.padding(horizontal = 4.dp)
+    ) {
+        Text(text = title, fontSize = 8.sp)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(text = subtitle, fontSize = 6.sp)
+    }
 }
+
+@Composable
+fun InformationAboutPublication(views: String, publicationTime: String, sentiment: String) {
+    Row(modifier = Modifier.padding(horizontal = 4.dp)) {
+        Text("$views views -", fontSize = 6.sp)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(" $publicationTime hours ago -", fontSize = 6.sp)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(" AI sentiment: $sentiment", fontSize = 6.sp)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WeatherTheme {
-        Greeting("Android")
+        Column {
+            InformationItemNews(
+                "How AI is shaping the future of business",
+                "By Sarah E.Needleman, Wall Street Journal"
+            )
+            InformationAboutPublication(
+                views = "1.2k",
+                publicationTime = "3 hours",
+                sentiment = "Negative"
+            )
+        }
+
     }
 }
